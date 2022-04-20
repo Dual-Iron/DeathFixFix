31f319c5b9b0c90f06d8219a6d9e3063c94d20f5
Dual <31146412+Dual-Iron@users.noreply.github.com>
2022-04-19 20:03:22 -0400

    Initial commit

---
+++ src/PluginState.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx.Logging;
+
+namespace RainWorldPlugin
+{
+    public sealed class PluginState
+    {
+        public PluginState(ManualLogSource logger)
+        {
+            Logger = logger;
+        }
+
+        public ManualLogSource Logger { get; }
+    }
+}
